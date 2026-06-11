0bfdf6ae2fef8565c8de8a253a2ad4708f6417ba
Datta Wakale <dattawakale2005@gmail.com>
2026-06-11 21:58:49 +0530

    valueChanges used debounce()
    Use subscribe for observable data

--- src/app/booking/booking.ts
+++ src/app/booking/booking.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
-
 import { BookingService } from '../services/booking.service';
 import { SportSelection } from './sport-selection/sport-selection';
 import { VenueSelection } from './venue-selection/venue-selection';
@@ -10,83 +9,47 @@ import { PaymentSelection } from './payment-selection/payment-selection';
 
 @Component({
   selector: 'app-booking',
-  imports: [
-    SportSelection,
-    VenueSelection,
-    SlotSelection,
-    BookingSummary,
-    PaymentSelection,
-    RouterLink
-  ],
+  imports: [ SportSelection,VenueSelection,SlotSelection,BookingSummary,PaymentSelection,RouterLink],
   templateUrl: './booking.html',
   styleUrl: './booking.scss'
 })
 export class Booking implements OnInit {
-
   user: any = null;
-
-  steps: any[] = [
-    'Sport',
-    'Venue',
-    'Slot',
-    'Summary',
-    'Payment'
-  ];
-  
+  steps: any[] = [ 'Sport', 'Venue', 'Slot', 'Summary', 'Payment'];
   currentStep:number = 1;
-
   constructor(
     public bookingService: BookingService,
     private router: Router
   ) {}
-
   ngOnInit(): void {
-
     // Subscribe to BehaviorSubject
     this.bookingService.currentStep
       .subscribe(step => {
-
         this.currentStep = step;
-
       });
 
     // get loggedInUser from localStorage
-    const storedUser =
-      localStorage.getItem('loggedInUser');
+    const storedUser =localStorage.getItem('loggedInUser');
 
     // if not there redirect to login first
     if (!storedUser) {
-
       this.router.navigate(['/login']);
-
       return;
-
     }
-
     // parse the value of user
-    this.user =
-      JSON.parse(storedUser);
-
+    this.user = JSON.parse(storedUser);
   }
-
   // logout the user
   logout(): void {
-
     // remove the user from localStorage
     localStorage.removeItem('loggedInUser');
-
     // reset step
     this.bookingService.setStep(1);
-
     this.router.navigate(['/home']);
-
   }
-
   // previous step
   prevStep(): void {
-
     if (this.currentStep > 1) {
-
       this.bookingService.setStep(
         this.currentStep - 1
       );
--- src/app/listofsports/listofsports.ts
+++ src/app/listofsports/listofsports.ts
@@ -1,102 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-
-interface Sport {
-  id: string;
-  name: string;
-  route: string;
-  colorClass: string;
-}
-
-interface Venues {
-  id: string;
-  name: string;
-  route: string;
-  colorClass: string;
-}
+import { FormControl, ReactiveFormsModule} from '@angular/forms';
+import { debounceTime, distinctUntilChanged} from 'rxjs';
+import { Sport } from '../shared/interfaces/sport.interface';
+import { Venue } from '../shared/interfaces/venue.interface';
+import { SPORTS } from '../shared/constants/sports.constants';
+import { VENUES } from '../shared/constants/venues.contstants';
 
 @Component({
   selector: 'app-list-of-sports',
-  imports: [RouterLink, FormsModule],
+  imports: [ RouterLink, ReactiveFormsModule],
   templateUrl: './listofsports.html',
-  styleUrls: ['./listofsports.scss'],
+  styleUrl: './listofsports.scss'
 })
-export class ListOfSportsComponent {
-  // array of objects
-  sports: Sport[] = [
-    { id: 'football', name: 'Football', route: '/booking', colorClass: 'football' },
-    { id: 'cricket', name: 'Cricket', route: '/booking', colorClass: 'cricket' },
-    { id: 'badminton', name: 'Badminton', route: '/booking', colorClass: 'badminton' },
-    { id: 'tennis', name: 'Tennis', route: '/booking', colorClass: 'tennis' },
-    { id: 'basketball', name: 'Basketball', route: '/booking', colorClass: 'basketball' },
-    { id: 'turf', name: 'Turf Booking', route: '/sports/turf', colorClass: 'turf' }
-  ];
-
-  venues: Venues[] = [
-    { id: 'madhapur', name: 'Madhapur', route: '/booking', colorClass: 'football' },
-    { id: 'hi-tech', name: 'Hi-Tech', route: '/booking', colorClass: 'cricket' },
-    { id: 'ameerpet', name: 'Ameerpet', route: '/booking', colorClass: 'badminton' },
-    { id: 'hafisguda', name: 'Hafisguda', route: '/booking', colorClass: 'tennis' },
-    { id: 'madhura-nagar', name: 'Madhura-nagar', route: '/booking', colorClass: 'basketball' },
-    { id: 'begumpet', name: 'Begumpet', route: '/sports/turf', colorClass: 'turf' }
-  ];
-
-  searchSport = '';
-  searchVenue = '';
-
+export class ListOfSportsComponent implements OnInit {
+ //  data for sports and venues
+  sports: Sport[] = SPORTS;
+  venues: Venue[] = VENUES;
   filteredSports: Sport[] = [...this.sports];
-  filteredVenues: Venues[] = [...this.venues];
-
-  sportVenueMap: Record<string, string[]> = {
-    football: ['Madhapur', 'Begumpet', 'Hi-Tech'],
-    cricket: ['Ameerpet', 'Madhapur'],
-    badminton: ['Hafisguda', 'Begumpet'],
-    tennis: ['Madhura-nagar', 'Hi-Tech'],
-    basketball: ['Madhapur'],
-    turf: ['Begumpet', 'Madhura-nagar']
-  };
-  // search sports you want to play
-  searchSports() {
-
-    const value = this.searchSport.toLowerCase().trim();
-
-    if (!value) {
-      this.filteredSports = [...this.sports];
-      this.filteredVenues = [...this.venues];
-      return;
-    }
-
-    this.filteredSports = this.sports.filter(
-      sport => sport.name.toLowerCase().includes(value)
-    );
-
-    const selectedSport = this.sports.find(
-      sport => sport.name.toLowerCase() === value
-    );
-
-    if (selectedSport) {
-
-      const venueNames =
-        this.sportVenueMap[selectedSport.id] || [];
-
-      this.filteredVenues = this.venues.filter(
-        venue => venueNames.includes(venue.name)
-      );
-    }
-  }
-
-  searchVenues() {
-
-    const value = this.searchVenue.toLowerCase().trim();
-
-    if (!value) {
-      this.filteredVenues = [...this.venues];
-      return;
-    }
-
-    this.filteredVenues = this.venues.filter(
-      venue => venue.name.toLowerCase().includes(value)
-    );
+  filteredVenues: Venue[] = [...this.venues];
+  searchControl = new FormControl('');
+  // on component load
+  ngOnInit(): void {
+    // Listen to search input changes
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      // Subscribe to search input changes
+      .subscribe(value => {
+        const searchValue = value?.toLowerCase().trim() || '';
+        // Empty input
+        if (!searchValue) {
+          this.filteredSports = [...this.sports];
+          this.filteredVenues = [...this.venues];
+          return;
+        }
+        // Filter sports
+        this.filteredSports = this.sports.filter(
+          sport =>
+            sport.name
+              .toLowerCase()
+              .includes(searchValue)
+        );
+        // Find exact sport
+        const selectedSport = this.sports.find(
+          sport =>
+            sport.name
+              .toLowerCase() === searchValue
+        );
+        if (selectedSport) {
+          // Show venues for that sport
+          this.filteredVenues = this.venues.filter(
+            venue =>
+              venue.sportId === selectedSport.id
+          );
+        }
+        
+        else {
+          this.filteredVenues = [];
+        }
+      });
   }
 }
